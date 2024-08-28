a7e6f2b6d3ebf11d955ef9113c45b5d77342a40a
Bellinna Uong <147825322+Bellinna-Uong@users.noreply.github.com>
2024-08-29 01:27:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// CHANGEMENT DE THEME
+const themeToggler = document.querySelector(".theme-switch__checkbox");
+
+themeToggler.addEventListener('change', () => {
+    document.body.classList.toggle('dark-theme-variables');
+});
